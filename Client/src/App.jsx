49d4422d9dd83f64fda546415e1850db8874de38
Cochrane <w0468594@campus.nscc.ca>
2024-11-07 20:28:59 -0400

    CA-21

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,4 @@
-import './App.css'
-import HomePage from './components/HomePage'
 import Login from './components/Login'
-import Register from './components/Register'
 import {Routes , Route } from 'react-router-dom'
 
 
@@ -10,8 +7,6 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/register' element={<Register/>}/>
       <Route path='/login' element={<Login/>}/>
     </Routes>
      
