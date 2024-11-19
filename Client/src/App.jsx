45f225eeb816c444bdde98de7d3344af5b0840be
Lord Sheriff <lordsheriff15@gmail.com>
2024-11-20 00:40:35 +0100

    Chatroom layout done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,19 @@
-import HomePage from './components/HomePage'
-import Register from './components/Register'
-import {Routes , Route } from 'react-router-dom'
+import ChatRoom from './components/ChatRoom';
+import HomePage from './components/HomePage';
+import Register from './components/Register';
 
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/register' element={<Register/>}/>
-    </Routes>
-     
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/chat' element={<ChatRoom />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Client/src/components/HomePage.jsx
+++ Client/src/components/HomePage.jsx
@@ -12,12 +12,20 @@ const HomePage = () => {
           We are excited to have you here. Please register to get started and
           enjoy all our features.
         </p>
-        <Link
-          to="/register"
-          className="inline-block bg-indigo-600 text-white font-semibold py-2 px-6 rounded-lg shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-        >
-          Register
-        </Link>
+        <div className="flex space-x-4 justify-center">
+          <Link
+            to="/register"
+            className="inline-block bg-indigo-600 text-white font-semibold py-2 px-6 rounded-lg shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Register
+          </Link>
+          <Link
+            to="/chat"
+            className="inline-block bg-green-500 text-white font-semibold py-2 px-6 rounded-lg shadow hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-offset-2"
+          >
+            Go to Chat Room
+          </Link>
+        </div>
       </div>
     </div>
   );
