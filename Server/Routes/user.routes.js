b9b4f1e62a05ebdb511e5459fd58b50a66507b82
Manthanp724 <136997064+Manthanp724@users.noreply.github.com>
2024-11-20 12:30:34 -0400

    Delete Server directory

--- Server/Routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { HandleRegister } = require('../Controllers/user.controllers');
-
-const router = express.Router()
-
-router.post("/register" , HandleRegister)
-
-
-module.exports = router;
